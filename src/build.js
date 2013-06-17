b8406094f820e5d87d1c56bd0ac412878a318cf5
jankuca <jan@jankuca.com>
2013-06-17 16:26:52 +0200

    add MixinLoader

--- src/build.js
+++ src/build.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var LessGenerator = require('./less-generator');
+var MixinLoader = require('./mixin-loader');
 
 
 var MIXIN_DIR = path.resolve(__dirname, '../mixins');
@@ -17,31 +18,32 @@ if (fs.existsSync(header_path)) {
 
 chunks.push(LessGenerator.generateGlobalToggles());
 
-var mixin_keys = fs.readdirSync(MIXIN_DIR).sort();
-mixin_keys.forEach(function (mixin_key) {
-  var mixin_path_noext = path.join(MIXIN_DIR, mixin_key, mixin_key);
+var loader = new MixinLoader(MIXIN_DIR);
+var mixin_descs = loader.getMixinDescriptors();
 
-  if (fs.existsSync(mixin_path_noext + '.less')) {
-    var less = fs.readFileSync(mixin_path_noext + '.less', 'utf8');
+mixin_descs.forEach(function (desc) {
+  switch (desc.type) {
+  case 'less':
+    var less = fs.readFileSync(desc.path, 'utf8');
     less = less.trim();
-
     chunks.push(less);
-    return;
-  }
+    break;
 
-  if (fs.existsSync(mixin_path_noext + '.js')) {
-    var mixin = require(mixin_path_noext + '.js');
-    var generator = new LessGenerator(mixin, mixin_key);
+  case 'js':
+    var mixin = require(desc.path);
+    var generator = new LessGenerator(mixin, desc.key);
 
     chunks.push(generator.generate());
-    return;
+    break;
   }
 });
+
 
 var footer_path = path.join(MIXIN_DIR, 'footer.less');
 if (fs.existsSync(footer_path)) {
   var footer = fs.readFileSync(footer_path, 'utf8');
   chunks.push(footer.trim());
 }
+
 
 console.log(chunks.join('\n\n'));
